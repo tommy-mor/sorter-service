baea2d6347be040d179f793e8e2b96b3152f9fe5
tommy <thmorriss@gmail.com>
2023-01-16 17:59:29 -0500

    is now a shared library..

--- src/main/ssorter/rank.clj
+++ src/main/ssorter/rank.clj
@@ -1,5 +1,14 @@
 (ns ssorter.rank
-  (:require [coffi.mem :as mem]
-            [coffi.ffi :as ffi]))
+  (:require [coffi.mem :as mem :refer [defalias]]
+            [coffi.ffi :as ffi :refer [defcfn]]))
+
+(defcfn strlen
+  "Given a string, measures its length in bytes."
+  strlen [::mem/c-string] ::mem/long)
+
+(ffi/load-library "pagerank/pr.so")
+
+(strlen "hello")
+;; => 5
 
 
