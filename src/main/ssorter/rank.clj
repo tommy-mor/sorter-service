2b0fa7e75e76dcacaf60937624b44e97f6641a64
tommy <thmorriss@gmail.com>
2023-01-16 19:16:49 -0500

    pr goes in resources

--- src/main/ssorter/rank.clj
+++ src/main/ssorter/rank.clj
@@ -1,12 +1,24 @@
 (ns ssorter.rank
-  (:require [coffi.mem :as mem :refer [defalias]]
-            [coffi.ffi :as ffi :refer [defcfn]]))
+  (:require [clojure.java.shell :refer [sh]]))
+
+(def pr-location )
+
+
+(sh "")
 
 (defcfn strlen
   "Given a string, measures its length in bytes."
   strlen [::mem/c-string] ::mem/long)
 
 (ffi/load-library "pagerank/pr.so")
+
+(ffi/find-symbol "pagerank_delta_function")
+
+(ffi/reify-libspec {:pg "pagerank/pr.so"})
+
+(defcfn pagerank_delta
+  "Given a string, measures its length in bytes."
+  pagerank_delta [] ::mem/long)
 
 (strlen "hello")
 ;; => 5
