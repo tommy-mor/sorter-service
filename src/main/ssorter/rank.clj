5f6052b9e986a37d28043d38190575ca0572cc9f
tommy <thmorriss@gmail.com>
2023-01-16 19:24:48 -0500

    robustly move the executable into cwd

--- src/main/ssorter/rank.clj
+++ src/main/ssorter/rank.clj
@@ -1,8 +1,21 @@
 (ns ssorter.rank
-  (:require [clojure.java.shell :refer [sh]]))
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.java.io :as io]
+            [taoensso.timbre :as log]))
 
-(def pr-location )
-
+(def pr-location (delay (let [loc (java.io.File. "./pr")]
+                          (if (.exists loc)
+                            loc
+                            (let [res (io/file (io/resource "pr"))]
+                              (assert (.exists res))
+                              
+                              (log/info "moving pr executable out of resources and into cwd")
+                              
+                              (with-open [in (io/input-stream res)
+                                          out (io/output-stream loc)]
+                                (io/copy in out))
+                              
+                              (.setExecutable loc true))))))
 
 (sh "")
 
