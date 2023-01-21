a7dd96905240e1ae8f9010a476c4ed20e758e7ce
tommy <thmorriss@gmail.com>
2023-01-21 15:44:42 -0500

    done with access control and user groups

--- resources/migrations/20230121174908-initial.up.sql
+++ resources/migrations/20230121174908-initial.up.sql
@@ -1,7 +1,41 @@
 ALTER TABLE users ADD created_at TIMESTAMP NOT NULL DEFAULT NOW();
 --;;
+
+-- dokuwiki: none, read, edit, create, upload, delete
+-- my drawing. can_view, can_edit
+
+-- each resource has a single access record, by default going to 0, which is private to only owner.
+-- these are acitions that that each resource has control over.
+-- for more complex actions (can_vote, can_suggest,
+-- can_add) or that action has its own row in the table
+ --       (default defers to edit permission of resource)
+
+
+CREATE TABLE access (
+  id SERIAL PRIMARY KEY,
+
+  inherit BOOLEAN NOT NULL, -- if inherit is true, nulls defer to parent. otherwise, nulls = false
+
+  -- roles/users/groups are ANDed together, only one needs to exist
+  role_id SERIAL REFERENCES roles(id),
+  user_id varchar(20) REFERENCES users(user_name),
+  group_id SERIAL REFERENCES groups(id),
+
+
+
+  owner BOOLEAN, -- can delete/change permissions/transfer owner. if its an action, owner=true means you can do the action
+  can_edit BOOLEAN, 
+  can_read BOOLEAN,
+
+  _and SERIAL REFERENCES access(id),
+
+  CHECK (role_id IS NOT NULL OR user_id IS NOT NULL OR group_id IS NOT NULL)
+)
+--;;
 CREATE TABLE groups (
 	   id SERIAL PRIMARY KEY,
+	   access SERIAL REFERENCES access(id), -- edit access allows for changing roles and users in roles
+
 	   user_id varchar(20) REFERENCES users(user_name) NOT NULL ON DELETE CASCADE,
 	   title TEXT NOT NULL,
 	   description TEXT NOT NULL,
@@ -9,6 +43,7 @@ CREATE TABLE groups (
 --;;
 CREATE TABLE roles (
 	   id SERIAL PRIMARY KEY,
+
 	   group_id SERIAL REFERENCES groups(id) NOT NULL,
 	   title TEXT NOT NULL,
 
@@ -23,4 +58,4 @@ CREATE TABLE users_in_groups (
 
 	   PRIMARY KEY (user_id, group_id, role_id)
 )
-
+--;;
