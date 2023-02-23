7ceb7a785d1c924670713044cd134e343209a3a9
tommy <thmorriss@gmail.com>
2023-02-23 12:32:39 -0500

    removed..

--- launch.sh
+++
@@ -1,9 +0,0 @@
-cd database && gnome-terminal -- ./run-postgres.sh
-cd -
-gnome-terminal -- bb js
-
-emacs --eval "(load-theme 'tsdh-light)" --file ./src/main/ssorter/client/ui.cljs  --eval "(call-interactively 'cider-connect-cljs)" &
-
-emacs --eval "(call-interactively 'cider-jack-in)" --file ./src/main/ssorter/rank.clj &
-
-echo "done"
