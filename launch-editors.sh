ff89e13086c3100e19b67eb0249d45e57d0be539
tommy <thmorriss@gmail.com>
2023-02-23 12:36:00 -0500

    switched order..

--- launch-editors.sh
+++ launch-editors.sh
@@ -4,6 +4,8 @@ gnome-terminal -- bb js
 
 emacs --eval "(load-theme 'tsdh-light)" --file ./src/main/ssorter/client/ui.cljs  --eval "(call-interactively 'cider-connect-cljs)" &
 
-emacs --eval "(call-interactively 'cider-jack-in)" --file ./src/main/ssorter/rank.clj &
+emacs --file ./src/main/ssorter/rank.clj --eval "(call-interactively 'cider-jack-in)" &
+
+
 
 echo "done"
