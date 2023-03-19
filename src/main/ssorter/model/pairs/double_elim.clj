e878317900b1e00a45e171da3c5e1278d637e4a1
tommy-mor <thmorriss@gmail.com>
2023-03-19 02:12:01 -0400

    double elim pair chosing strategy

---
+++ src/main/ssorter/model/pairs/double_elim.clj
@@ -0,0 +1,16 @@
+(ns ssorter.model.pairs.double-elim
+  (:require
+   [ssorter.server-components.db :refer [exec!]]
+   [taoensso.timbre :as log]
+   [honey.sql.helpers :as h]
+   [com.wsscode.pathom3.connect.built-in.resolvers :as pbir]
+   [com.wsscode.pathom3.connect.operation :as pco]))
+
+
+
+
+
+
+
+
+(defn testfn [x] (+ x 3))
---
+++ src/test/ssorter/model/pairs/double_elim.clj
@@ -0,0 +1,12 @@
+(ns ssorter.model.pairs.double-elim
+  (:require
+   [clojure.test :refer [deftest is]]
+   [ssorter.model.pairs.double-elim :as sut]
+   [ssorter.model.tags :as m.tags]
+   [ssorter.model.membership :as m.membership]
+   [clojure.string :as str]
+   [mount.core :as mount]
+   [ssorter.server-components.pathom :refer [build-parser]]))
+
+
+(sut/testfn 33)
